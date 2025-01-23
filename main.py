36488e3108255c6e06a5e9797425fe715aaa2ee5
SokolovNick <165019746+SokolovNick@users.noreply.github.com>
2025-01-23 20:58:14 +0600

    changing images

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import pygame
 import random
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 760
+SCREEN_HEIGHT = 760
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Shooting Gallery")
 icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target_duck.png")
-target_width = 210
+background_img = pygame.image.load("img/space_background.png")
+target_img = pygame.image.load("img/target_ufo.png")
+
+target_width = 118
 target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (0, 191, 255)
-
 running = True
 while running:
-    screen.fill(color)
+    screen.blit(background_img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
