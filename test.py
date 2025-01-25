1e523ac83d8f9bbb92d4975ad9104c48725bd7a6
SokolovNick <165019746+SokolovNick@users.noreply.github.com>
2025-01-25 13:01:19 +0600

    replacing mouse to scope

--- test.py
+++ test.py
@@ -13,15 +13,17 @@ pygame.display.set_icon(icon)
 
 background_img = pygame.image.load("img/space_background.png")
 target_img = pygame.image.load("img/target_ufo.png")
-crosshair_img = pygame.image.load("img/crosshair.png")
+crosshair_img = pygame.image.load("img/crosshair.png")  # Загрузите изображение прицела
 
 # Получите размеры изображений
 target_width = target_img.get_width()
 target_height = target_img.get_height()
 
+# Инициализируйте позицию цели
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+# Скрыть системный курсор мыши
 pygame.mouse.set_visible(False)
 
 running = True
@@ -36,10 +38,13 @@ while running:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    # Получите текущую позицию курсора мыши
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
+    # Отобразите цель
     screen.blit(target_img, (target_x, target_y))
 
+    # Отобразите прицел в центре курсора
     crosshair_offset_x = crosshair_img.get_width() // 2
     crosshair_offset_y = crosshair_img.get_height() // 2
     screen.blit(crosshair_img, (mouse_x - crosshair_offset_x, mouse_y - crosshair_offset_y))
