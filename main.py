1c83bc3f077abc6052c985fab7ebcc6205ebf786
SokolovNick <165019746+SokolovNick@users.noreply.github.com>
2025-01-26 21:23:23 +0600

    add explosions after hits

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
@@ -20,6 +21,7 @@ pygame.display.set_icon(icon)
 background_img = pygame.image.load("img/space_background.png")
 target_img = pygame.image.load("img/target_ufo.png")
 crosshair_img = pygame.image.load("img/crosshair.png")
+explosion_img = pygame.image.load("img/explosion.png")  # Загрузка изображения взрыва
 
 target_width = target_img.get_width()
 target_height = target_img.get_height()
@@ -28,6 +30,10 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 pygame.mouse.set_visible(False)
+
+# Список для отслеживания активных взрывов
+explosions = []
+explosion_duration = 1.5  # Длительность отображения взрыва в секундах
 
 running = True
 while running:
@@ -38,13 +44,27 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                # Добавляем новый взрыв в список
+                explosions.append((target_x, target_y, time.time()))
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+    current_time = time.time()
 
+    # Обновляем и отображаем взрывы
+    for explosion in explosions[:]:
+        explosion_x, explosion_y, explosion_start_time = explosion
+        if current_time - explosion_start_time < explosion_duration:
+            screen.blit(explosion_img, (explosion_x, explosion_y))
+        else:
+            # Удаляем взрывы, которые больше не нужно отображать
+            explosions.remove(explosion)
+
+    # Отображаем цель
     screen.blit(target_img, (target_x, target_y))
 
+    # Отображаем прицел
+    mouse_x, mouse_y = pygame.mouse.get_pos()
     crosshair_offset_x = crosshair_img.get_width() // 2
     crosshair_offset_y = crosshair_img.get_height() // 2
     screen.blit(crosshair_img, (mouse_x - crosshair_offset_x, mouse_y - crosshair_offset_y))
